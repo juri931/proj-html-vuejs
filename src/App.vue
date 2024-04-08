fe7e43e042c41fd012342256fe643505dd7c4c0c
juri931 <impa.mat@hotmail.it>
2024-04-08 13:04:28 +0200

    [add] header

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
 import Header from "./components/partials/Header.vue";
 import Main from "./components/partials/Main.vue";
 import Footer from "./components/partials/Footer.vue";
+export default {
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
+};
 </script>
 
 <template>
--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -3,7 +3,20 @@ export default {};
 </script>
 
 <template>
-  <div class="container">HEADER</div>
+  <nav>
+    <div class="container">
+      <img src="/public/img/white-logo-2.png" alt="Logo" />
+      <ul>
+        <li><a id="home" href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a class="btn" id="sign-in" href="#">Sign In</a></li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <style lang="scss" scoped>
