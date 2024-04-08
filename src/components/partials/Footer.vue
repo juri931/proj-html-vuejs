91ca93ce18accec55fbb666336517dd5cdbd5643
juri931 <impa.mat@hotmail.it>
2024-04-08 14:29:26 +0200

    [add] footer

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -3,7 +3,74 @@ export default {};
 </script>
 
 <template>
-  <h1>FOOTER</h1>
+  <body>
+    <div class="container top">
+      <div class="left col-4">
+        <img src="/public/img/white-logo-2.png" alt="Logo" />
+        <p>
+          When, while lovely valley teems vapour aroundmeand meridian sun
+          strikes the upper impenetrable
+        </p>
+      </div>
+
+      <div class="right col-8">
+        <ul>
+          <li>Quick Links</li>
+          <li><a href="">Services</a></li>
+          <li><a href="">Contact</a></li>
+          <li><a href="">Blog</a></li>
+        </ul>
+
+        <ul>
+          <li>Resources</li>
+          <li><a href="">Art Design</a></li>
+          <li><a href="">Computer</a></li>
+          <li><a href="">IT News</a></li>
+          <li><a href="">Networking</a></li>
+          <li><a href="">Web Security</a></li>
+        </ul>
+
+        <ul>
+          <li>Contact Us</li>
+          <li>
+            <p>
+              <span>Address: </span> Patricia C. Amedee 4401 Waldeck Street
+              Grapevine Nashville, Tx 76051
+            </p>
+          </li>
+          <li>
+            <p><span>Phone: </span>+99 (0) 101 0000 888</p>
+          </li>
+          <li>
+            <p><span>Email: </span>info@yourdomain.com</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="line"></div>
+
+    <div class="container bottom">
+      <div class="left">© 2020 Phlox. All Rights Reserved.</div>
+
+      <div class="logos">
+        <ul>
+          <li>
+            <a href=""><i class="fa-brands fa-instagram"></i></a>
+          </li>
+          <li>
+            <a href=""><i class="fa-brands fa-linkedin-in"></i></a>
+          </li>
+          <li>
+            <a href=""><i class="fa-brands fa-facebook-f"></i></a>
+          </li>
+          <li>
+            <a href=""><i class="fa-brands fa-twitter"></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </body>
 </template>
 
 <style lang="scss" scoped>
