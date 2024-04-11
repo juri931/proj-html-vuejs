ce2e90bf4f67a5e11bdd130e3e0d5c9566525a22
juri931 <impa.mat@hotmail.it>
2024-04-11 17:09:20 +0200

    [fix]

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  const path = require('path')
-
-
-  root: path.resolve(__dirname, 'src'),
-  server: {
-    port: 8080,
-    hot: true
-  }
-
 });
